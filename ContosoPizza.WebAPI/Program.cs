4c60ab725ad62315239510f6a141e0dd9bc01d18
Jürgen Heiling <juergen.heiling@gmail.com>
2024-01-06 11:13:00 +0100

    added https://learn.microsoft.com/en-gb/training/modules/build-web-api-aspnet-core/5-exercise-add-data-store

--- ContosoPizza.WebAPI/Program.cs
+++ ContosoPizza.WebAPI/Program.cs
@@ -22,17 +22,13 @@ var summaries = new[]
 };
 
 app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
+    Enumerable.Range(1, 5).Select(index => new WeatherForecast
+    (
+        DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        Random.Shared.Next(-20, 55),
+        summaries[Random.Shared.Next(summaries.Length)]
+    ))
+    .ToArray())
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
